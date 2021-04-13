495053cbcd90d2c83351e62341138d25d8c6b38d
Abhishek <abhishekgupta1599@gmail.com>
2021-04-13 12:31:55 +0530

    3d models added and animated.

--- src/script.js
+++ src/script.js
@@ -224,16 +224,18 @@ import {OrbitControls} from '../helperScripts/OrbitControls.js'
 import {GLTFLoader} from '../helperScripts/GLTFLoader.js'
 import * as dat from '../node_modules/dat.gui/build/dat.gui.module.js'
 
-let scene, camera, renderer,controls, planes,planes1,geometry,material,plane,plane1,geometry1,material1
-let clock,time,delta,direction,speed,direction1,speed1
-
+let scene, camera, renderer,controls, planes,geometry,material,plane
+let clock,time,delta,direction,speed
+let HouseModels = []
+let TreeModels = []
+let brickApartments = []
 const gui = new dat.GUI()
 const container = document.getElementById( 'container' );
 
 scene = new THREE.Scene();
-// scene.fog = new THREE.FogExp2( 0xf0fff0, 0.14 );
+scene.fog = new THREE.Fog('#d5c7e8',1,700)
 camera = new THREE.PerspectiveCamera(30,window.innerWidth/window.innerHeight,0.1,1000)
-// camera.position.set(0,0,20)
+
 
 
 
@@ -248,7 +250,9 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.shadowMap.enabled = true;//enable shadow
 container.appendChild( renderer.domElement );
 
-
+const light = new THREE.AmbientLight( 0x404040, 3 ); // soft white light
+scene.add( light );
+scene.add( light );
 
 window.onresize = function () {
 
@@ -260,24 +264,98 @@ window.onresize = function () {
 };
 
 
+const pos_to_neg = (x) => {
+  return -Math.abs(x)
+}
+
+const neg_to_pos = (x) => {
+  return Math.abs(x)
+}
+
+
+
 //Texture
 
 const textureLoader = new THREE.TextureLoader()
 let modelLoader =  new GLTFLoader();
 
-modelLoader.load('./assets/3d/house/scene.gltf',(gltf)=>{
-  // scene.add(gltf.scene)
-  // console.log(gltf)
-  renderer.render(scene,camera)
-})
+const modelURLS = {
+  house:'./assets/3d/house/scene.gltf',
+  trees:'./assets/3d/trees/scene.gltf',
+  brickApartment:'./assets/3d/brickApartment/scene.gltf'
+}
+
+
+for( let i = 0; i < 5 ; i++)    //HOUSE
+{
+  modelLoader.load('./assets/3d/house/scene.gltf',(gltf)=>{
+    let r;
+    const pos = {
+      x : 80,
+      y:-10,
+      z:-1000 * i
+    }
+    if(i % 2 == 0){
+      r = pos_to_neg(pos.x)
+      pos.x = r
+    } else {
+      r = neg_to_pos(pos.x)
+      pos.x = r
+    }
+    gltf.scene.children[0].position.set(pos.x ,pos.y, pos.z )
+    gltf.scene.children[0].scale.x = gltf.scene.children[0].scale.y = gltf.scene.children[0].scale.z = 8
+    HouseModels.push(gltf.scene.children[0])
+    scene.add(gltf.scene.children[0])
+    renderer.render(scene,camera)
+    
+  })
+}
+
+for( let i = 0; i < 10 ; i++)   // TREES
+{
+  modelLoader.load(modelURLS.trees,(gltf)=>{
+    let r;
+    gltf.scene.children[0].scale.x = gltf.scene.children[0].scale.y = gltf.scene.children[0].scale.z = 0.1
+    const pos = {
+      x:-45,
+      y:-10,
+      z:-933*i
+    }
+    if(i % 2 == 0){
+      r = pos_to_neg(pos.x)
+      pos.x = r
+    } else {
+      r = neg_to_pos(pos.x)
+      pos.x = r
+    }
+    gltf.scene.children[0].position.set(pos.x,pos.y,pos.z )
+    TreeModels.push(gltf.scene.children[0])
+    scene.add(gltf.scene.children[0])
+    renderer.render(scene,camera)
+  })
+}
+
+
+// for( let i = 0; i < 3 ; i++)   // BrickApartment
+// {
+//   modelLoader.load(modelURLS.brickApartment,(gltf)=>{
+//     console.log(gltf.scene.children[0])
+//     // gltf.scene.children[0].scale.x = gltf.scene.children[0].scale.y = gltf.scene.children[0].scale.z = 0.004 
+//     // gltf.scene.children[0].position.set(-5 ,-1, -3 * i )
+//     // leftTreeModels.push(gltf.scene.children[0])
+//     // scene.add(gltf.scene.children[0])
+//     renderer.render(scene,camera)
+//   })
+// }
+
 
 controls = new OrbitControls(camera, renderer.domElement);
   // controls.minDistance = 500;  
-    // controls.maxDistance = 500;
+    controls.maxDistance = 500;
     controls.update()
 
   planes = [];
-  planes1 = []
+
 
   //Main Road geometry
   let mainRoads = []
@@ -314,18 +392,6 @@ controls = new OrbitControls(camera, renderer.domElement);
     
   }
   
-  //MIDDLE BARS GEOMETRY
-  // for(let i = 0; i < 1 ;i++){
-
-  //   geometry1 = new THREE.PlaneBufferGeometry(5,40,1)
-  //   material1 = new THREE.MeshBasicMaterial({color:'#FFFFFF'}) 
-  //   geometry1.rotateX(Math.PI * 1.5)
-  //   plane1 = new THREE.Mesh(geometry1, material1);
-  //   plane1.position.set(0, -8, 200 - i * 100);
-  //   scene.add(plane1)
-  //   planes1.push(plane1)
-  // }
-
 
 clock = new THREE.Clock();
 time = 0
@@ -333,8 +399,6 @@ delta = 0; // Road Delta
 direction = new THREE.Vector3(0, 0, 1);// Road Direction
 speed = 50; //units a second..Road Speed
 
-direction1 = new THREE.Vector3(0,0,3) // Middle Bars Direction
-speed1 = speed - 10 //Middle Bars Speed
 
 const render = () => {
     requestAnimationFrame(render);
@@ -346,15 +410,27 @@ const render = () => {
         if (plane.position.z > 100) plane.position.z =  ((plane.position.z - 650) % 700); // IF ROAD BREAKS CHANGE -650 to more negative value..
     });
 
-    // planes1.forEach((plane)=>{
-    //   plane1.position.addScaledVector(direction1, speed1 * delta);
-    //   if (plane1.position.z > 100) plane1.position.z = -300 + ((plane1.position.z - 100) % 200);
-    // })
+   
 
 
     mainRoads.forEach((mainRoad)=>{
       mainRoad.position.addScaledVector(direction,speed*delta)
       if (mainRoad.position.z > 100) mainRoad.position.z =  ((mainRoad.position.z - 650) % 700);
+    })
+
+
+    HouseModels.forEach((HouseModel)=>{
+      HouseModel.position.addScaledVector(direction,speed*delta)
+      if (HouseModel.position.z > 1000 ) {
+        HouseModel.position.z = -1000
+      }
+    })
+
+    TreeModels.forEach((TreeModel)=>{
+      TreeModel.position.addScaledVector(direction,(speed -10) * delta)
+      if (TreeModel.position.z > 933){
+        TreeModel.position.z = -933
+      }
     })
     
     renderer.render(scene, camera);
